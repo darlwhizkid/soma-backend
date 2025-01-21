f46018e4b268919670f45828ff7c22524c08b4ec
darlwhizkid <darlingtonodom@gmail.com>
2025-01-21 12:09:11 +0100

    added css

---
+++ somahairacademy/assets/js/reviews.js
@@ -0,0 +1,4 @@
+document.getElementById('writeReviewBtn').addEventListener('click', () => {
+  console.log('Button clicked'); // Add this line
+  document.getElementById('reviewForm').style.display = 'block';
+});
